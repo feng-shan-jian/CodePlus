e82285f7236ac79a490d0fe81d64dd0bf2a49b40
feng-shan-jian <139083171+feng-shan-jian@users.noreply.github.com>
2026-07-01 20:11:23 +0800

    feat(commands): add completion support

---
+++ codeplus/commands/__init__.py
@@ -0,0 +1,24 @@
+
+from codeplus.commands.loader import load_user_commands
+from codeplus.commands.parser import complete, parse_command
+from codeplus.commands.registry import (
+    Command,
+    CommandContext,
+    CommandHandler,
+    CommandRegistry,
+    CommandType,
+    UIController,
+)
+
+
+__all__ = [
+    "Command",
+    "CommandContext",
+    "CommandHandler",
+    "CommandRegistry",
+    "CommandType",
+    "UIController",
+    "complete",
+    "load_user_commands",
+    "parse_command",
+]
